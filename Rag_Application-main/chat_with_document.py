5c37635b21203ef522ba43024a360eb95dd850f7
Naitri Doshi <jesaldoshi123@gmail.com>
2024-04-01 17:11:19 +0530

    added prompt template

--- Rag_Application-main/chat_with_document.py
+++ Rag_Application-main/chat_with_document.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+from constants import CHROMA_SETTINGS
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import LlamaCpp
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain.prompts import PromptTemplate
 from transformers import AutoModel
 from numpy.linalg import norm
 
@@ -23,33 +27,56 @@ persist_directory = os.environ.get('PERSIST_DIRECTORY')
 divice_type = os.environ.get('DIVICE_TYPE')
 model_path = os.environ.get('MODEL_PATH')
 model_n_ctx = os.environ.get('MODEL_N_CTX')
-model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+model_n_batch = int(os.environ.get('MODEL_N_BATCH', 8))
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS', 4))
 
-from constants import CHROMA_SETTINGS
 
 def main():
     # Parse the command line arguments
     args = parse_arguments()
-    embeddings = HuggingFaceInstructEmbeddings(model_name=embeddings_model_name)
-    chroma_client = chromadb.PersistentClient(settings=CHROMA_SETTINGS , path=persist_directory)
-    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS, client=chroma_client)
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name=embeddings_model_name)
+    chroma_client = chromadb.PersistentClient(
+        settings=CHROMA_SETTINGS, path=persist_directory)
+    db = Chroma(persist_directory=persist_directory, embedding_function=embeddings,
+                client_settings=CHROMA_SETTINGS, client=chroma_client)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    
+
     # Prepare the LLM
     match divice_type:
         case "CPU":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx,
+                           n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case "GPU":
-            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_gpu_layers = 90 , n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+            llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_gpu_layers=90,
+                           n_batch=model_n_batch, callbacks=callbacks, verbose=False)
         case _default:
             # raise exception if model_type is not supported
-            raise Exception(f"Divice type {divice_type} is not supported. Please choose one of the following: CPU , GPU ")
-            
-    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
-    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
+            raise Exception(
+                f"Divice type {divice_type} is not supported. Please choose one of the following: CPU , GPU ")
+
+    model = AutoModel.from_pretrained(
+        'jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
+
+    # Prompt
+    template = """You are a video recommender system that help users to find videos to answer their queries.
+    Use the following pieces of context to answer the question at the end.
+    For each question, suggest three videos, with a short description of it and
+    If you don't know the answer, just say that you don't know, don't try to make up
+    {context}
+    Question: {question}
+    Your response:"""
+
+    QA_CHAIN_PROMPT = PromptTemplate(
+        input_variables=["context", "question"],
+        template=template,
+    )
+    chain_type_kwargs = {"prompt": QA_CHAIN_PROMPT}
+
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever,
+                                     return_source_documents=not args.hide_source, chain_type_kwargs=chain_type_kwargs)
     # Interactive questions and answers
     while True:
         query = input("\nEnter a query: ")
@@ -61,25 +88,26 @@ def main():
         # Get the answer from the chain
         start = time.time()
         res = qa(query)
-        answer, docs = res['result'], [] if args.hide_source else res['source_documents']
+        answer, docs = res['result'], [
+        ] if args.hide_source else res['source_documents']
         end = time.time()
 
         # Print the result
-        cos_sim = lambda a,b: (a @ b.T) / (norm(a)*norm(b))
+        def cos_sim(a, b): return (a @ b.T) / (norm(a)*norm(b))
         print("\n\n> Question:")
         print(query)
         print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        em = model.encode([answer,docs[0].page_content])
+        em = model.encode([answer, docs[0].page_content])
 
-        if (cos_sim(em[0], em[1])>0.80):
-           print(answer)
-           print("\n> " + docs[0].metadata["source"] + ":")
-           print(docs[0].page_content)
-           score = cos_sim(em[0], em[1])
-           print("\n> "+f"Score :- {score}")
+        if (cos_sim(em[0], em[1]) > 0.80):
+            print(answer)
+            print("\n> " + docs[0].metadata["source"] + ":")
+            print(docs[0].page_content)
+            score = cos_sim(em[0], em[1])
+            print("\n> "+f"Score :- {score}")
         else:
-           print("we have no such data regading your query, sorry !!")
-        
+            print("we have no such data regading your query, sorry !!")
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Ask questions to your documents without an internet connection, '
